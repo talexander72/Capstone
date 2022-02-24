1dbbbbede82e18576be646829ad4139f8969cf0b
Tucker Alexander <talexander72@gatech.edu>
2022-02-24 02:47:08 -0500

    added progress bar for reading audio files

--- landing_page.py
+++ landing_page.py
@@ -1,24 +1,24 @@
 import PySimpleGUI as sg
 from scipy.io.wavfile import read
 import os
-import pyACA
+#import pyACA
 
 
-def features(feature, files):
-    specList = []
-    for i in files:
-        fs = i[0]
-        x = i[1]
-        [vsf, t] = pyACA.computeFeature(feature, x, fs)
-        specList.append([vsf, t])
-    return specList
+#def features(feature, files):
+#    specList = []
+#    for i in files:
+#        fs = i[0]
+#        x = i[1]
+#        [vsf, t] = pyACA.computeFeature(feature, x, fs)
+#        specList.append([vsf, t])
+#    return specList
 
 
-fList = ['SpectralCentroid', 'SpectralCrestFactor', 'SpectralDecrease', 'SpectralFlatness', 'SpectralFlux',
-         'SpectralKurtosis', 'SpectralMfccs', 'SpectralPitchChroma', 'SpectralRolloff', 'SpectralSkewness',
-         'SpectralSlope', 'SpectralSpread', 'SpectralTonalPowerRatio', 'TimeAcfCoeff', 'TimeMaxAcf',
-         'TimePeakEnvelope', 'TimePredictivityRatio', 'TimeRms', 'TimeStd', 'TimeZeroCrossingRate']
-
+#fList = ['SpectralCentroid', 'SpectralCrestFactor', 'SpectralDecrease', 'SpectralFlatness', 'SpectralFlux',
+#         'SpectralKurtosis', 'SpectralMfccs', 'SpectralPitchChroma', 'SpectralRolloff', 'SpectralSkewness',
+#         'SpectralSlope', 'SpectralSpread', 'SpectralTonalPowerRatio', 'TimeAcfCoeff', 'TimeMaxAcf',
+#         'TimePeakEnvelope', 'TimePredictivityRatio', 'TimeRms', 'TimeStd', 'TimeZeroCrossingRate']
+BAR_MAX = 1000
 sg.theme('Black')
 layout = \
     [[sg.Text("Path to Folder:"), sg.Input(key="-IN-", change_submits=True), sg.FolderBrowse(key="-IN-")],
@@ -26,7 +26,9 @@ layout = \
     [sg.Text('Enter a Chunk Size (samples)'), sg.Slider(range=(100, 500), default_value=222, size=(40, 20), orientation='horizontal')],
     [sg.Text('Enter a Hop Size (samples)'), sg.Slider(range=(100, 500), default_value=500, size=(40, 20), orientation='horizontal')],
     [sg.Text('Preprocessing: Would You Like to Cross Validate?)'), sg.Radio('Yes', "RADIO1"), sg.Radio('No', "RADIO1", default=True)],
-    [sg.Button('Ok'), sg.Button('Cancel')]]
+    [sg.Button('Ok'), sg.Button('Cancel')],
+    [[sg.Text('file read progress:')],
+    [sg.ProgressBar(BAR_MAX, orientation='h', size=(20, 20), key='-PROG-')]]]
 
 window = sg.Window('Enhanced Feature Selection', layout)
 
@@ -40,18 +42,19 @@ while True:
         classes = os.listdir(values["-IN-"])
         entries1 = os.listdir(values["-IN-"] + '/' + classes[1])
         entries2 = os.listdir(values["-IN-"] + '/' + classes[2])
-
+        total = len(entries1) + len(entries2)
+        BAR_MAX = total
         classA = []
-        for i in entries1:
-            [fs, x] = read(values["-IN-"] + '/' + classes[1] +'/' + str(i))
-            classA.append([fs, x[:, 2]])
-
         classB = []
-        for i in entries2:
-            [fs, x2] = read(values["-IN-"] + '/' + classes[2] +'/' + str(i))
-            classB.append([fs, x[:, 2]])
-
-
+        for k in range(total):
+            if k >= len(entries1):
+                [fs, x2] = read(values["-IN-"] + '/' + classes[2] +'/' + entries2[k-len(entries1)])
+                classB.append([fs, x[:, 2]])
+                window['-PROG-'].update(k+1)
+            else:
+                [fs, x] = read(values["-IN-"] + '/' + classes[1] +'/' + entries1[k])
+                classA.append([fs, x[:, 2]])
+                window['-PROG-'].update(k+1)
 
 
 window.close()
--- progress_bar.py
+++ progress_bar.py
@@ -1,19 +1,54 @@
 import PySimpleGUI as sg
+import os
+from scipy.io.wavfile import read
 
-# progress bar
+#progress bar
 
 sg.theme('Black')
+
 BAR_MAX = 100
-layout = [[sg.Text('A custom progress meter')],
+
+# layout the Window
+layoutB = [[sg.Text('custom progress')],
           [sg.ProgressBar(BAR_MAX, orientation='h', size=(20,20), key='-PROG-')],
           [sg.Cancel()]]
 
-window = sg.Window('Custom Progress Meter', layout)
+# create the Window
+#window = sg.Window('Custom Progress Meter', layout)
+# loop that would normally do something useful
+#for i in range(1000):
+    # check to see if the cancel button was clicked and exit loop if clicked
+#    event, values = window.read(timeout=10)
+#    if event == 'Cancel' or event == sg.WIN_CLOSED:
+#        break
+        # update bar with loop value +1 so that bar eventually reaches the maximum
+#    window['-PROG-'].update(i+1)
 
-for i in range(1000):
-    event, values = window.read(timeout=10)
+#window.close()
+entries1 = os.listdir('/Users/tuckeralexander/Desktop/Classes/Capstone/data/MIMII/fan/id_00/normal')
+entries2 = os.listdir('/Users/tuckeralexander/Desktop/Classes/Capstone/data/MIMII/fan/id_00/abnormal')
+del(entries1[-1])
+del(entries2[-1])
+classA = []
+classB = []
+count=0
+print(len(entries1)+len(entries2))
+window2 = sg.Window('progress', layoutB)
+
+for k in range(len(entries1)+len(entries2)):
+    event, values = window2.read(timeout=10)
     if event == 'Cancel' or event == sg.WIN_CLOSED:
         break
-    window['-PROG-'].update(i+1)
+    [fs, x] = read("/Users/tuckeralexander/Desktop/Classes/Capstone/data/MIMII/fan/id_00/normal/" + entries1[k])
+    classA.append([fs, x[:, 2]])
+    count = count+1
+    window2['-PROG-'].update(k + 1)
 
-window.close()
+    while k >= len(entries1):
+        [fs, x2] = read("/Users/tuckeralexander/Desktop/Classes/Capstone/data/MIMII/fan/id_00/abnormal/" + entries2[k-len(entries1)])
+        classB.append([fs, x[:, 2]])
+        count = count+1
+        window2['-PROG-'].update(k + 1)
+    #if divmod(count,2) == 0:
+        #window2['-PROG-'].update(k + 1)
+window2.close()
