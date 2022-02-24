2ee6e372340a10812864ae18cfaa6e298cdc0a45
talexander72 <78112431+talexander72@users.noreply.github.com>
2022-02-23 22:50:02 -0500

    Create main.py
    fix master branch

---
+++ main.py
@@ -0,0 +1,23 @@
+import PySimpleGUI as sg
+
+sg.theme('Black')
+
+layout = \
+    [[sg.Text("Path to Folder:"), sg.Input(key="-IN-", change_submits=True), sg.FolderBrowse(key="-IN-")],
+    [sg.Text('Preprocessing: before feature extraction, audio must be "chunked" into equal size segments')],
+    [sg.Text('Enter a Chunk Size (samples)'), sg.Slider(range=(100, 500), default_value=222, size=(40, 20), orientation='horizontal')],
+    [sg.Text('Enter a Hop Size (samples)'), sg.Slider(range=(100, 500), default_value=500, size=(40, 20), orientation='horizontal')],
+    [sg.Text('Preprocessing: Would You Like to Cross Validate?)'), sg.Radio('Yes', "RADIO1"), sg.Radio('No', "RADIO1", default=True)],
+    [sg.Button('Ok'), sg.Button('Cancel')]]
+
+window = sg.Window('Enhanced Feature Selection', layout)
+#test comment branch
+while True:
+    event, values = window.read()
+    print(values["-IN-"])
+    if event == sg.WIN_CLOSED or event == "Exit" or event == 'Cancel':
+        break
+    elif event == "Ok":
+        print("You chose a chunk size of %s samples, with cross validation = %s" % (values[0], values[2]))
+
+window.close()
