dd9b46d47394dc14cc56595d104f48bd4b8c1918
Tucker Alexander <talexander72@gatech.edu>
2022-03-10 01:22:23 -0500

    added feature extraction progress bar

--- landing_page.py
+++ landing_page.py
@@ -40,21 +40,22 @@ layout = \
     [[sg.Text("Path to Folder:"), sg.Input(key="-IN-", change_submits=True), sg.FolderBrowse(key="-IN-")],
     [sg.Text('__'*30)],
     [sg.Text('Preprocessing Pipeline: before feature extraction, audio must be "chunked" into equal size segments')],
-    [sg.Text('Enter a Chunk Size (samples)'), sg.Slider(range=(256, 8192), default_value=2048, size=(40, 20), orientation='horizontal')],
-    [sg.Text('Enter an Overlap Ratio (percent)'), sg.Slider(range=(0, 99), default_value=0, size=(40, 20), orientation='horizontal')],
+    [sg.Text('Enter a Chunk Size (samples)'), sg.Slider(range=(256, 8192), key="-CHUNK-", default_value=2048, size=(40, 20), orientation='horizontal')],
+    [sg.Text('Enter an Overlap Ratio (percent)'), sg.Slider(range=(0, 99), key="-HOP-", default_value=0, size=(40, 20), orientation='horizontal')],
     [sg.Text('Smaller datasets may benefit from cross validation. Implement K-Fold Cross Validation?)'), sg.Radio('Yes', "RADIO1"), sg.Radio('No', "RADIO1", default=True)],
-    [sg.Text('(Step 1 of 3) Launch Preprocessing Pipeline:'), sg.Button('Ok'), sg.Button('Cancel')],
+    [sg.Text('(Step 1 of 3) Launch Preprocessing Pipeline:'), sg.Button('Ok', key="-READ-"), sg.Button('Cancel', key="-READX-")],
     [sg.Text('__'*30)],
     [sg.Text('Feature Extraction: Audio features are computed and averaged across chunks')],
     [sg.Text('How many features would you like to test?'), sg.Slider(range=(1,10), default_value=10, size=(40,28), orientation='horizontal')],
-    [sg.Text('(Step 2 of 3) Launch Feature Extraction:'), sg.Button('Ok', key="-FEAT-"), sg.Button('Cancel')]]
+    [sg.Text('(Step 2 of 3) Launch Feature Extraction:'), sg.Button('Ok', key="-FEAT-"), sg.Button('Cancel', key="-FEATX-")],
+    [sg.Text('__'*30)]]
 
 window = sg.Window('Enhanced Feature Selection', layout, element_justification='c')
-
-
+featuresbool = False
 while True:
     event, values = window.read()
-    if event == "Ok":
+    if event == "-READ-":
+        featuresbool = True
         classes = os.listdir(values["-IN-"])
         entries1 = os.listdir(values["-IN-"] + '/' + classes[1])
         entries2 = os.listdir(values["-IN-"] + '/' + classes[2])
@@ -69,27 +70,22 @@ while True:
                 break
             if i >= len(entries1):
                 [fs, x2] = read(values["-IN-"] + '/' + classes[2] + '/' + entries2[i-len(entries1)])
-                framesB = chunker(x2[:, 2], values[0], values[1])
+                framesB = chunker(x2[:, 2], values["-CHUNK-"], values["-HOP-"])
                 classB.append([fs, x2[:, 2]])
                 chunksB.append(framesB)
                 i = i + 1
             else:
                 [fs, x] = read(values["-IN-"] + '/' + classes[1] + '/' + entries1[i])
-                framesA = chunker(x[:, 2], values[0], values[1])
+                framesA = chunker(x[:, 2], values["-CHUNK-"], values["-HOP-"])
                 classA.append([fs, x[:, 2]])
                 chunksA.append(framesA)
                 i = i + 1
-    elif event == sg.WIN_CLOSED or event == "Exit" or event == 'Cancel':
+    elif event == "-FEAT-" and featuresbool:
+        for i in range(total):
+            if not sg.one_line_progress_meter('File Read Progress', i+1, total, 'step 2 of 3: extracting features'):
+                break
+    elif event == sg.WIN_CLOSED or event == "Exit" or event == '-READX-' or event == '-FEATX-':
         break
+
+
 window.close()
-
-layout2 = \
-    [[sg.Text("Feature Selection:"), sg.Input(key="-IN-", change_submits=True), sg.FolderBrowse(key="-IN-")],
-    [sg.Text('__'*30)],
-    [sg.Text('Preprocessing: before feature extraction, audio must be "chunked" into equal size segments')],
-    [sg.Text('Enter a Chunk Size (samples)'), sg.Slider(range=(100, 500), default_value=222, size=(40, 20), orientation='horizontal')],
-    [sg.Text('Enter an Overlap Ratio (percent)'), sg.Slider(range=(1, 99), default_value=99, size=(40, 20), orientation='horizontal')],
-    [sg.Text('Smaller datasets may benefit from cross validation. Implement K-Fold Cross Validation?)'), sg.Radio('Yes', "RADIO1"), sg.Radio('No', "RADIO1", default=True)],
-    [sg.Button('Ok'), sg.Button('Cancel')]]
-
-window2 = sg.Window('Enhanced Feature Selection', layout2, element_justification='c')
